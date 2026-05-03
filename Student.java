4dfbd8ddcb920202f4c8f2eac573973c5a190668
Jax-rgb <jacquescarigma@gmail.com>
2026-05-04 00:20:42 +0800

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,31 @@
+public class Student implements EducationalUnit {
+    private String name;
+    private String studentId;
+    private double tuitionFee;
+
+    public Student(String name, String studentId, double tuitionFee) {
+        this.name = name;
+        this.studentId = studentId;
+        this.tuitionFee = tuitionFee;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void displayDetails(String indent) {
+        System.out.println(indent + "Student: " + name + " | ID: " + studentId + " | Tuition: $" + tuitionFee);
+    }
+
+    @Override
+    public int getTotalStudents() {
+        return 1;
+    }
+
+    @Override
+    public double getBudget() {
+        return -tuitionFee;  // Revenue (negative cost)
+    }
+}
